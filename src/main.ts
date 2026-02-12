936daed8d2623591083666ee522e5e1b1737fcb0
Your Name <blacksmith199e@gmail.com>
2026-02-12 12:33:12 +0100

    enable cors for vercel

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://tage-frontend.vercel.app'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
